e221795338889df58bba6ed7641791f3b43cabcd
a1093350 <100494607+a1093350@users.noreply.github.com>
2022-03-16 23:47:37 +0800

    Add files via upload
    kending homework

--- index.php
+++ index.php
@@ -1,14 +1,57 @@
 <html>
 
+<meta name="keywords" content="墾丁, 三日遊">
+<title>墾丁三日遊</title>
+<link rel="icon" href="/img/kending2.jpg" type="image/x-icon" />
 <head></head>
-
-<body>
+<body bgcolor="#dddddd">
 <?php
-	echo "hello php";
-    echo "<h1>8787</h1>";
-    echo "<br>";
     echo date("Y,m,d");
+    echo "<h1>   </h1>";
+    echo "<br>";
 ?>
+<center><h1>墾丁三日遊</h1></center>
+<h2>  </h2>
+<h3>  </h3>
+<hr color="black" width="100%"/>
+<center><h2><b>1.行程內容</h2><br/>
+<h3>墾丁好好玩，陽光海水比基尼</h3></b><br/>
+<marquee>好讚</marquee>
+<a href="https://www.ktnp.gov.tw/" target="new"><img src="/img/kending2.jpg" border="2" width="400" height="300"></a>
+<a href="https://www.agoda.com/city/kenting-tw.html?cid=1844104" target="new"><img src="/img/kending3.jpg" border="2" width="400" height="300"></a></center>
+<br/><br/>
+<hr color="black" width="100%"/> 
+<center>
+ <h2><b>2.填寫表單</h2><br/>
+ <h3><b>
+輸入名字<input type="text" name="uName" value="" placeholder="輸入中文" required><br/><br/>
+輸入信箱<input type="email" name="na"><br/><br/>
+手機號碼<input type="tel" name="te"><br/><br/>
+性別: Male<input type="radio" name="mra">Female<input type="radio" name="fra"><br/><br/>
+飲食: 葷<input type="checkbox"name="meatch">素<input type="checkbox"name="vegch"><br/><br/>
+<form>
+T-shirt size
+<select name="T-shirt尺寸">
+<option value="Taipei">S</option>
+<option value="Taoyuan">M</option>
+<option value="Hsinchu">L</option>
+<option value="Miaoli">XL</option>
+</select>
+</form>
+T-shirt 顏色<input type="color" name="co"><br/><br/>
+出生日期<input type="date" name="da"><br/><br/>
+票數<input type="number" name="num"><br/><br/>
+<input type="submit"></br><br/>
+</b></h3>
+<hr color="black" width="100%"/> 
+<table width="300" border="1">
+    <tr><th>尺寸表</th><th>S</th><th>M</th><th>L</th><th>XL</th></tr>
+    <tr><td>長</td><td>68.5</td><td>71</td><td>73.5</td><td>76</td></tr>
+    <tr><td>寬</td><td>47</td><td>52</td><td>55</td><td>57</td></tr>
+    <tr><td>高</td><td>150~160</td><td>160~170</td><td>171-175</td><td>175-180</td></tr>
+</table>
+
+
 </body>
 
 </html>
